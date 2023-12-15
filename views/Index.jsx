af474ec8b5b5684ffd829280b1574fbadbbd243d
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-12-14 22:20:28 -0500

    Added dynamic anchor tags to index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -6,12 +6,14 @@ function Index(props) {
         backgroundColor: '#000000',
         };
 
+    
+
   return (
     <>
         <h1 style={myStyle}>See All The Pokemon</h1>
         <ul>
-            {props.pokemon.map((pokemon)=>{
-                return<li>{pokemon.name}</li>
+            {props.pokemon.map((pokemon, i)=>{
+                return<li><a href={`/pokemon/${i}`}>{pokemon.name}</a></li>
             })}
         </ul>
         
