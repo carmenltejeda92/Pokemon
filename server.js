a221a222898404142b1a9f2a8a393113e3d0b6c0
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-12-14 20:00:58 -0500

    All my files are created

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+
+
+
+app.get('/pokemon', (req, res)=>{
+    res.send()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('listening ...')
+})
