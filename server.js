b1eaa01e2434b55f0d5c4caf5ddda1210e5ee916
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-12-15 01:08:20 -0500

    New page is able to be viewed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const pokemen = require('./models/pokemon')
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 app.use((req, res, next)=>{
     console.log('I run for all routes')
     next()
@@ -21,25 +21,24 @@ app.get('/pokemon', (req, res)=>{
 })
 
 
-app.get('/pokemon/:shownumber', (req, res)=>{
-    res.render('Show', {pokemon: pokemen[req.params.shownumber]})
-})
-
 app.get('/pokemon/new', (req, res)=>{
     res.render('New')
 })
 
 
 
-app.post('/new',(req, res)=>{
-    addEventListener("submit", (e)=>{
-        onsubmit=(e)=>{
-            
-        }
-    })
+app.post('/pokemon',(req, res)=>{
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        pokemen.push()
+    }
+    console.log(req.body)
+    res.redirect('/pokemon')
 })
 
-
+app.get('/pokemon/:shownumber', (req, res)=>{
+    res.render('Show', {pokemon: pokemen[req.params.shownumber]})
+})
 
 
 
--- views/New.jsx
+++ views/New.jsx
@@ -4,10 +4,10 @@ function New() {
   return (
     <>
       <h1>Create A New Pokemon</h1>
-      <form action="/pokemon" method="POST"><br/>
-        Name: <input style={{margin:"1em"}} type="text" name="name" /><br/>
+      {/* <form action="/pokemon" method="POST"><br/>
+        Name: <input style={{margin:"1em"}} type="text" /><br/>
         <input style={{margin:"1em"}} type="submit" value="Create Pokemon" />
-      </form>
+      </form> */}
     </>
   )
 }
