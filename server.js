85165dfbb64bcc91351c40c759fb0fb93d0efd2e
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-12-14 21:45:27 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const pokemon = require('./models/pokemon')
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+// app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res)=>{
     res.send('Welcome to the Pokemon App!')
@@ -9,11 +12,8 @@ app.get('/', (req, res)=>{
 
 
 app.get('/pokemon', (req, res)=>{
-    res.send(pokemon)
+    res.render('Index')
 })
-
-
-
 
 
 
---
+++ views/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Index() {
+    const myStyle = {
+        color: '#ffffff',
+        backgroundColor: '#000000',
+        };
+
+  return (
+    <>
+        <h1 style={myStyle}>See All The Pokemon</h1>
+        
+    </>
+  )
+}
+
+export default Index
